8675d377d8b8c28d4c7cc33c06b787a51d7ee9b2
Anton Pavliuk <streetreadger@gmail.com>
2024-01-10 11:54:43 +0200

    Half of the book

---
+++ src/bin/trait_object.rs
@@ -0,0 +1,52 @@
+use std::mem::{align_of, size_of};
+
+trait Test {
+    fn add(&self) -> i32;
+    fn sub(&self) -> i32;
+    fn mul(&self) -> i32;
+}
+
+#[repr(C)]
+struct FatPointer<'a> {
+    data: &'a mut Data,
+    vtable: *const usize,
+}
+
+struct Data {
+    a: i32,
+    b: i32,
+}
+
+fn add(s: &Data) -> i32 {
+    s.a + s.b
+}
+
+fn sub(s: &Data) -> i32 {
+    s.a - s.b
+}
+
+fn mul(s: &Data) -> i32 {
+    s.a * s.b
+}
+
+fn main() {
+    let mut data = Data { a: 3, b: 2 };
+
+    let vtable = vec![
+        0,
+        size_of::<Data>(),
+        align_of::<Data>(),
+        add as usize,
+        sub as usize,
+        mul as usize,
+    ];
+
+    let fat_pointer = FatPointer {
+        data: &mut data,
+        vtable: vtable.as_ptr(),
+    };
+    let test = unsafe { std::mem::transmute::<FatPointer, &dyn Test>(fat_pointer) };
+    println!("Add: 3 + 2 = {}", test.add());
+    println!("Sub: 3 - 2 = {}", test.sub());
+    println!("Mul: 3 * 2 = {}", test.mul());
+}
